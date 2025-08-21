5bfac66b7b68b98c05964a62567f37d9715f4754
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2025-08-21 09:53:27 +0530

    Update video.tsx

--- components/styledcomps/video.tsx
+++ components/styledcomps/video.tsx
@@ -1,53 +1,54 @@
 "use client";
-import {
-  MediaController,
-} from "media-chrome/react";
-import { cn } from "@/lib/utils";
-import { useEffect, useRef } from "react";
+import Link from "next/link";
+import Aboutdata from "@/data/about.json";
+import { Heading } from "@/components/styledcomps/heading";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { P } from "../ui/p";
+import { Container } from "../styledcomps/container";
+// import R2Image from "../styledcomps/R2Image";
+import VideoPlayer from "../styledcomps/video";
 
-interface VideoPlayerProps {
-  src: string;
-  muted?: boolean;
-  className?: string;
-}
-
-export default function VideoPlayer({
-  src = "https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4",
-  className,
-  muted = true,
-  ...props
-}: VideoPlayerProps) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    // Ensure video attributes are set after component mounts
-    if (videoRef.current) {
-      videoRef.current.tabIndex = -1;
-      // Try to play the video manually
-      videoRef.current.play().catch((err) => {
-        console.log("Video autoplay prevented:", err);
-      });
-    }
-  }, []);
-
+export default function AboutAlt() {
   return (
-    <MediaController 
-      className={cn(className)} 
-      suppressHydrationWarning={true}
-      {...props}
+    <Container
+      className={`relative my-20 before:absolute before:content-['']
+            before:h-full before:w-full before:top-0 before:left-0 before:bg-[rgba(0, 0, 0, 0.329)] before:opacity-60`}
+      animate
     >
-      <video 
-        ref={videoRef}
-        slot="media" 
-        src={src} 
-        preload="auto" 
-        muted={muted} 
-        autoPlay 
-        loop
-        tabIndex={-1}
-        suppressHydrationWarning={true}
-        playsInline
-      />
-    </MediaController>
+      <div className="">
+        <VideoPlayer
+          src="https://pub-3d943afeed9643318d31712e02ebf613.r2.dev/North-by-Northeast-tours-Northeast-India.mp4"
+          poster="https://pub-3d943afeed9643318d31712e02ebf613.r2.dev/videoposter.jpg"
+          className="z-0 object-cover object-center"
+        />
+
+        <div className="absolute top-1/2 right-0 -translate-y-1/2 z-10">
+          <div className="grid lg:grid-cols-6">
+            <div className="md:col-end-7 md:col-span-3">
+              <div>
+                <Card className="bg-gray-950/70 border-0">
+                  <CardHeader>
+                    <Heading variant="sectiontitlehalf">
+                      <span className="text-white">
+                        {Aboutdata.about.title}
+                      </span>
+                    </Heading>
+                  </CardHeader>
+                  <CardContent>
+                    <P className="text-justify text-white mb-8">
+                      {Aboutdata.about.text}
+                    </P>
+                    <Link href={Aboutdata.about.btnlink} className="mt-2">
+                      <Button>{Aboutdata.about.btntext}</Button>
+                    </Link>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 }
